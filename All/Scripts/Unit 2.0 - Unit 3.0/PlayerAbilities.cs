6f8ea37d6e1739567a013b3093c8e595580890b4
cwfreemanj <50161832+cwfreemanj@users.noreply.github.com>
2021-05-20 17:42:17 -0700

    Updated Player Abilities

--- All/Scripts/Unit 2.0 - Unit 3.0/PlayerAbilities.cs
+++ All/Scripts/Unit 2.0 - Unit 3.0/PlayerAbilities.cs
@@ -6,6 +6,16 @@ public class PlayerAbilities : MonoBehaviour
 {
     private Animator anim;
 
+    public GameObject Attack1;
+
+    enum Abilities
+    {
+        Attack1,
+        Attack2,
+    }
+
+    Abilities selectedAbility = Abilities.Attack1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +24,19 @@ public class PlayerAbilities : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
+    {
         if (Input.GetKeyDown("1"))
         {
-            anim.SetBool("Attack1", true);
+            switch (selectedAbility) {
+                case Abilities.Attack1:
+                anim.SetBool("Attack1", true);
+                    GameObject temp = Instantiate(Attack1, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);
+                    temp.transform.rotation = transform.rotation;
+                   
+                    /*Ray rayout = new Ray(transform.position, transform.forward);
+                    temp.transform.position = Vector3.MoveTowards(transform.position, transform.forward, 100);*/
+                    break;
+        }
         }
         else
         {
